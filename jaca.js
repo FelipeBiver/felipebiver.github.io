2b1f32d9b4ee975751c1391408709cb0c7f106ba
FelipeBiver <12100757@aluno.cotemig.com.br>
2022-10-29 18:36:24 -0300

    dia2

--- jaca.js
+++ jaca.js
@@ -1,7 +1,32 @@
 function Mudarestado(el) {
     var display = document.getElementById(el).style.display;
     if(display == "none")
-        document.getElementById(el).style.display = 'block';
+    document.getElementById(el).style.display = 'block';
     else
-        document.getElementById(el).style.display = 'none';
+    document.getElementById(el).style.display = 'none';
 }
+var interval=0;
+var maxSlider = document.querySelectorAll('.box-image').length -1;
+
+
+
+
+acao();
+function acao(){
+
+let img =document.querySelectorAll('.box-image img')
+setInterval(function(){
+
+img[interval].style.display = 'none';
+interval++
+if(interval > maxSlider){
+    interval=0
+}
+img[interval].style.display = 'block'
+},8000)
+
+
+}
+
+
+
